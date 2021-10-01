67eaf5f903700870b619058e9e5f57bbc9bd061c
Tiffany McGillvary <lunamora425@gmail.com>
2021-10-01 16:42:22 -0400

    I fucked this all up lol, hopefully this fixes it

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // Require the necessary discord.js classes
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
-const fs = require('fs');
 const consolere = require('console-remote-client');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({
@@ -19,6 +17,14 @@ const client = new Client({
 	},
   });
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
   consolere.connect({
 	server: 'https://console.re', // optional, default: https://console.re
 	channel: 'FORTUNA-SERVER', // required
@@ -30,37 +36,24 @@ const client = new Client({
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready! Bot online!');
+	console.re.log('Ready! Bot online!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 // Login to Discord with your client's token
 client.login(token);
-
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-// Place your client and guild ids here
-const clientId = '891766698015875112';
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
-
-const rest = new REST({ version: '9' }).setToken(token);
-
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
-
-		console.log('Successfully reloaded application (/) commands.');
-	}
-	catch (error) {
-		console.error(error);
-	}
-})();
